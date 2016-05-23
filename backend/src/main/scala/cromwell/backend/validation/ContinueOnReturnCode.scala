fd7a2b33868bf83fede24940f1783bd50952cf9e
Thibault Jeandet <tjeandet@broadinstitute.org>
2016-05-23 15:37:31 -0400

    default runtime attributes

--- backend/src/main/scala/cromwell/backend/validation/ContinueOnReturnCode.scala
+++ backend/src/main/scala/cromwell/backend/validation/ContinueOnReturnCode.scala
@@ -1,5 +1,11 @@
 package cromwell.backend.validation
 
+import wdl4s.types.{WdlArrayType, WdlBooleanType, WdlIntegerType, WdlType}
+
+object ContinueOnReturnCode {
+  val validWdlTypes = Set[WdlType](WdlArrayType(WdlIntegerType), WdlBooleanType, WdlIntegerType)
+}
+
 /**
   * Decides if a call/job continues upon a specific return code.
   */
@@ -20,12 +26,14 @@ sealed trait ContinueOnReturnCode {
 
 /**
   * Continues based on a generic true / false flag, that when false, only zero return codes continue.
+  *
   * @param continue If true, all return codes are valid for continuing.
   */
 case class ContinueOnReturnCodeFlag(continue: Boolean) extends ContinueOnReturnCode
 
 /**
   * Continues only if the call/job return code is found in returnCodes.
+  *
   * @param returnCodes Inclusive set of return codes that specify a job success.
   */
 case class ContinueOnReturnCodeSet(returnCodes: Set[Int]) extends ContinueOnReturnCode
